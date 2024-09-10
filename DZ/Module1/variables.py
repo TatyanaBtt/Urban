fff07fb55ce19c236224e1798239b5153a5bdf49
TatyanaBtt <163028449+TatyanaBtt@users.noreply.github.com>
2024-09-10 20:13:51 +0300

    Add files via upload

---
+++ DZ/Module1/variables.py
@@ -0,0 +1,10 @@
+number_homework = 12
+number_hours = 1.5
+name_course = 'Python'
+time_for_one_task = number_hours/number_homework
+print(f"Курс:{name_course}, всего задач: {number_homework}, затрачено часов: {number_hours}, среднее время выполнения {time_for_one_task}  часа.")
+
+
+
+
+
