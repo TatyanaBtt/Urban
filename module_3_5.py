1a339d2f6d54a035ca49d04e883ef7c115582c46
TatyanaBtt <2269821@gmail.com>
2024-09-25 00:09:56 +0300

    add

--- module_3_5.py
+++ module_3_5.py
@@ -1,10 +1,11 @@
 def get_multiplied_digits(number):
-    str_number = str(number)
+    str_number = str(abs(number))
     first = int(str_number[0])
-    if int(len(str_number)) == 1:
+    number_1 = str_number.replace('0', '1')
+    if len(str_number) <= 1:
         return first
     else:
-        return first * get_multiplied_digits(int(str_number[1:]))
+        return first * get_multiplied_digits(int(number_1[1:]))
 
-rezalt1 = get_multiplied_digits(20450056)
+rezalt1 = get_multiplied_digits(-60203250)
 print(rezalt1)
